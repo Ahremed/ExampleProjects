b6c3f4de127689dd222d0a643babf328d258ab52
Ahremed <Kobets@meta.ua>
2016-02-18 15:10:16 +0200

    Add sample JFrame

---
+++ src/example/Okno/Main.java
@@ -0,0 +1,11 @@
+package example.Okno;
+
+/**
+ * Created by Vladyslav on 18.02.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Okno ok = new Okno();
+        ok.setVisible(true);
+    }
+}
---
+++ src/example/Okno/Okno.java
@@ -0,0 +1,9 @@
+package example.Okno;
+
+import javax.swing.*;
+
+/**
+ * Created by Vladyslav on 18.02.2016.
+ */
+public class Okno extends JFrame {
+}
